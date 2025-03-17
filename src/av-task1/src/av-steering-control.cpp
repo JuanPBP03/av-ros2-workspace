626f67890203548c463f2db381a714870e1fdca0
JuanPBP03 <juanpablobecerra103@gmail.com>
2025-03-16 22:11:59 -0400

    finished steering controller node

--- src/av-task1/src/av-steering-control.cpp
+++ src/av-task1/src/av-steering-control.cpp
@@ -1,30 +1,35 @@
 #include <chrono>
 #include <rclcpp/rclcpp.hpp>
 #include "std_msgs/msg/float32.hpp"
+#include "geometry_msgs/msg/point.hpp"
 #include <random>
 
 using namespace std::chrono_literals;
 
-constexpr auto FREQ = 10;
+constexpr auto FREQ = 5;
 constexpr auto MSG_PERIOD = 1.0s / FREQ;
 
-class speedPublisher : public rclcpp::Node {
+class steeringController : public rclcpp::Node {
   public:
-    speedPublisher() : Node("vehicle_speed_publisher") {
-      publisher_ = this->create_publisher<std_msgs::msg::Float32>("vehicle_speed", 10);
-      timer_ = this->create_wall_timer(MSG_PERIOD, std::bind(&speedPublisher::publishSpeed, this));
+    steeringController() : Node("steering_controller") {
+      publisher_ = this->create_publisher<std_msgs::msg::Float32>("steering_command", 10);
+      speed_subscriber_ = this->create_subscription<std_msgs::msg::Float32>("vehicle_speed", 10,
+         std::bind(&steeringController::speed_active,this));
+      position_subscriber_ = this->create_subscription<ge
     }
   private:
+  bool speed_active = false;
+  bool position_active = false;
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr speed_subscriber_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
   
-  void publishSpeed(){
-    std_msgs::msg::Float32 speed;
-    speed.data = getSpeed();
-    RCLCPP_INFO(this->get_logger(), "Vehicle Speed: %.2f", speed.data);
-    publisher_->publish(speed);
+  void speed_callback(){
+   speed_active = true;
   }
 
-  float getSpeed(){ 
-    return getRandomFloat(-100,100);
+  void position_callback(){ 
+    position_active = true;
   }
   
   float getRandomFloat(int min, int max){
@@ -33,15 +38,14 @@ class speedPublisher : public rclcpp::Node {
     static std::uniform_real_distribution<float> dist(min, max);
     return dist(gen);
   }
-  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_;
-  rclcpp::TimerBase::SharedPtr timer_;
+  
 };
 
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc,argv);
-  rclcpp::spin(std::make_shared<speedPublisher>());
+  rclcpp::spin(std::make_shared<steeringController>());
   rclcpp::shutdown();
   return 0;
 }
